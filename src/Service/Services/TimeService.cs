4ec08206dac791a1ef874e84a9745bebd71db2bd
Furkan Kara <frknkara@hotmail.com>
2021-11-04 03:53:50 +0300

    time service and unit tests

--- src/Service/Services/TimeService.cs
+++ src/Service/Services/TimeService.cs
@@ -1,22 +1,77 @@
-﻿using Service.Contracts;
+﻿using Model.Campaign;
+using Service.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Service.Services
 {
     public class TimeService : ITimeService
     {
         private readonly ITimeManager _timeManager;
+        private readonly ICampaignManager _campaignManager;
+        private readonly IProductManager _productManager;
 
-        public TimeService(ITimeManager timeManager)
+        public TimeService(ITimeManager timeManager, ICampaignManager campaignManager, IProductManager productManager)
         {
             _timeManager = timeManager;
+            _campaignManager = campaignManager;
+            _productManager = productManager;
         }
 
         public string IncreaseTime(int hour)
         {
+            if (hour <= 0)
+                throw new Exception("Time value is not valid.");
+
+            var oldTime = _timeManager.GetTimeValue();
+            List<CampaignDto> oldActiveCampaigns = _campaignManager.GetActiveCampaigns(oldTime);
+
             var updatedTime = _timeManager.IncreaseTimeValue(hour);
-            //TODO: inject campaign service, product service. get products in campaign and set prices by campaign discount
+            List<CampaignDto> currentActiveCampaigns = _campaignManager.GetActiveCampaigns(updatedTime);
+
+            SetNewPrices(hour, currentActiveCampaigns);
+
+            var closedCampaigns = GetClosedCampaigns(oldActiveCampaigns, currentActiveCampaigns);
+            SetPricesToInitials(closedCampaigns);
             var time = updatedTime % 24;
             return time.ToString("D2") + ":00";
+        }
+
+        private void SetNewPrices(int incrementHour, List<CampaignDto> campaigns)
+        {
+            campaigns.ForEach(x => _productManager.UpdateProductPrice(
+                x.ProductId,
+                GetNewPrice(incrementHour, x.ProductPrice, x.ProductInitialPrice, x.PriceManipulationLimit, x.Duration)));
+        }
+
+        private void SetPricesToInitials(List<CampaignDto> campaigns)
+        {
+            campaigns.ForEach(x => _productManager.UpdateProductPrice(x.ProductId, x.ProductInitialPrice));
+        }
+
+        private List<CampaignDto> GetClosedCampaigns(List<CampaignDto> oldActiveCampaigns, List<CampaignDto> currentActiveCampaigns)
+        {
+            return oldActiveCampaigns.Where(p => !currentActiveCampaigns.Any(l => p.Id == l.Id)).ToList();
+        }
+
+        private int GetDiscountAmount(int initialPrice, int maxLimit, int duration)
+        {
+            return (int)Math.Ceiling(initialPrice * ((double)maxLimit / 100) / duration);
+        }
+
+        private double GetMinPrice(int initialPrice, int maxLimit)
+        {
+            return initialPrice - (initialPrice * (double)maxLimit / 100);
+        }
+
+        private int GetNewPrice(int incrementHour, int currentPrice, int initialPrice, int maxLimit, int duration)
+        {
+            var discountAmount = incrementHour * GetDiscountAmount(initialPrice, maxLimit, duration);
+            var minPrice = GetMinPrice(initialPrice, maxLimit);
+            if (currentPrice - discountAmount < minPrice)
+                return (int)Math.Ceiling(minPrice);
+            return currentPrice - discountAmount;
         }
     }
 }
--- test/ServiceTests/Services/TimeServiceTests.cs
+++ test/ServiceTests/Services/TimeServiceTests.cs
@@ -1,6 +1,9 @@
+using Model.Campaign;
 using Moq;
 using Service.Contracts;
 using Service.Services;
+using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace ServiceTests
@@ -8,17 +11,119 @@ namespace ServiceTests
     public class TimeServiceTests
     {
         private Mock<ITimeManager> _mockTimeManager;
+        private Mock<ICampaignManager> _mockCampaignManager;
+        private Mock<IProductManager> _mockProductManager;
+        private Guid _product1;
+        private Guid _product2;
+        private List<CampaignDto> _oldCampaigns;
+        private List<CampaignDto> _currentCampaigns;
 
         public TimeServiceTests()
         {
             _mockTimeManager = new Mock<ITimeManager>();
+            _mockCampaignManager = new Mock<ICampaignManager>();
+            _mockProductManager = new Mock<IProductManager>();
+
+            _product1 = Guid.NewGuid();
+            _product2 = Guid.NewGuid();
+            _oldCampaigns = new List<CampaignDto>
+            {
+                new CampaignDto
+                {
+                    Id = Guid.NewGuid(),
+                    Duration = 10,
+                    Name = "campaign_1",
+                    ProductId = _product1,
+                    ProductCode = "product1",
+                    PriceManipulationLimit = 20,
+                    TargetSalesCount = 100,
+                    ProductInitialPrice = 100,
+                    CreationTime = 0,
+                    ProductPrice = 100
+                },
+                new CampaignDto
+                {
+                    Id = Guid.NewGuid(),
+                    Duration = 1,
+                    Name = "campaign_2",
+                    ProductId = _product2,
+                    ProductCode = "product2",
+                    PriceManipulationLimit = 40,
+                    TargetSalesCount = 50,
+                    ProductInitialPrice = 200,
+                    CreationTime = 0,
+                    ProductPrice = 200
+                },
+            };
+            _currentCampaigns = new List<CampaignDto>
+            {
+                new CampaignDto
+                {
+                    Id = Guid.NewGuid(),
+                    Duration = 10,
+                    Name = "campaign_1",
+                    ProductId = _product1,
+                    ProductCode = "product1",
+                    PriceManipulationLimit = 20,
+                    TargetSalesCount = 100,
+                    ProductInitialPrice = 100,
+                    CreationTime = 0,
+                    ProductPrice = 100
+                },
+            };
+        }
+
+        [Fact]
+        public void Test_IncreaseTime_Zero_Hour()
+        {
+            var service = new TimeService(_mockTimeManager.Object, _mockCampaignManager.Object, _mockProductManager.Object);
+            Exception exception = Assert.Throws<Exception>(() => service.IncreaseTime(0));
+            Assert.Equal("Time value is not valid.", exception.Message);
+        }
+
+        [Fact]
+        public void Test_IncreaseTime_Negative_Hour()
+        {
+            var service = new TimeService(_mockTimeManager.Object, _mockCampaignManager.Object, _mockProductManager.Object);
+            Exception exception = Assert.Throws<Exception>(() => service.IncreaseTime(-1));
+            Assert.Equal("Time value is not valid.", exception.Message);
+        }
+
+        [Fact]
+        public void Test_IncreaseTime()
+        {
+            _mockTimeManager.Setup(x => x.GetTimeValue()).Returns(0);
+            _mockTimeManager.Setup(x => x.IncreaseTimeValue(It.IsAny<int>())).Returns(2);
+
+            _mockCampaignManager.Setup(x => x.GetActiveCampaigns(0)).Returns(_oldCampaigns);
+            _mockCampaignManager.Setup(x => x.GetActiveCampaigns(2)).Returns(_currentCampaigns);
+
+            var service = new TimeService(_mockTimeManager.Object, _mockCampaignManager.Object, _mockProductManager.Object);
+            var result = service.IncreaseTime(2);
+            _mockTimeManager.Verify(x => x.GetTimeValue());
+            _mockCampaignManager.Verify(x => x.GetActiveCampaigns(0));
+            _mockTimeManager.Verify(x => x.IncreaseTimeValue(2));
+            _mockCampaignManager.Verify(x => x.GetActiveCampaigns(2));
+
+            //discount: 2hour * (100$ * 20% / 10hour) = 4 | new price: 100 - 4 = 96
+            _mockProductManager.Verify(x => x.UpdateProductPrice(_product1, 96));
+
+            //old campaigns set prices to initial prices
+            _mockProductManager.Verify(x => x.UpdateProductPrice(_product2, 200));
+
+            Assert.Equal("02:00", result);
         }
 
         [Fact]
         public void Test_IncreaseTime_01()
         {
+            _mockTimeManager.Setup(x => x.GetTimeValue()).Returns(0);
             _mockTimeManager.Setup(x => x.IncreaseTimeValue(It.IsAny<int>())).Returns(1);
-            var service = new TimeService(_mockTimeManager.Object);
+
+            var list = new List<CampaignDto>();
+            _mockCampaignManager.Setup(x => x.GetActiveCampaigns(0)).Returns(list);
+            _mockCampaignManager.Setup(x => x.GetActiveCampaigns(1)).Returns(list);
+            var service = new TimeService(_mockTimeManager.Object, _mockCampaignManager.Object, _mockProductManager.Object);
             var result = service.IncreaseTime(1);
             _mockTimeManager.Verify(x => x.IncreaseTimeValue(1));
             Assert.Equal("01:00", result);
@@ -27,20 +132,30 @@ namespace ServiceTests
         [Fact]
         public void Test_IncreaseTime_13()
         {
-            _mockTimeManager.Setup(x => x.IncreaseTimeValue(It.IsAny<int>())).Returns(13);
-            var service = new TimeService(_mockTimeManager.Object);
-            var result = service.IncreaseTime(1);
-            _mockTimeManager.Verify(x => x.IncreaseTimeValue(1));
-            Assert.Equal("13:00", result);
+            _mockTimeManager.Setup(x => x.GetTimeValue()).Returns(1);
+            _mockTimeManager.Setup(x => x.IncreaseTimeValue(13)).Returns(14);
+
+            var list = new List<CampaignDto>();
+            _mockCampaignManager.Setup(x => x.GetActiveCampaigns(1)).Returns(list);
+            _mockCampaignManager.Setup(x => x.GetActiveCampaigns(14)).Returns(list);
+            var service = new TimeService(_mockTimeManager.Object, _mockCampaignManager.Object, _mockProductManager.Object);
+            var result = service.IncreaseTime(13);
+            _mockTimeManager.Verify(x => x.IncreaseTimeValue(13));
+            Assert.Equal("14:00", result);
         }
 
         [Fact]
         public void Test_IncreaseTime_25()
         {
+            _mockTimeManager.Setup(x => x.GetTimeValue()).Returns(0);
             _mockTimeManager.Setup(x => x.IncreaseTimeValue(It.IsAny<int>())).Returns(25);
-            var service = new TimeService(_mockTimeManager.Object);
-            var result = service.IncreaseTime(1);
-            _mockTimeManager.Verify(x => x.IncreaseTimeValue(1));
+
+            var list = new List<CampaignDto>();
+            _mockCampaignManager.Setup(x => x.GetActiveCampaigns(0)).Returns(list);
+            _mockCampaignManager.Setup(x => x.GetActiveCampaigns(25)).Returns(list);
+            var service = new TimeService(_mockTimeManager.Object, _mockCampaignManager.Object, _mockProductManager.Object);
+            var result = service.IncreaseTime(25);
+            _mockTimeManager.Verify(x => x.IncreaseTimeValue(25));
             Assert.Equal("01:00", result);
         }
     }
